041943abb39ccaa3cab16589a47db694460f87d2
Saphal Shakha <66344649+Saphall@users.noreply.github.com>
2021-06-24 20:05:54 +0545

    improvements

--- main.py
+++ main.py
@@ -2,40 +2,31 @@ import discord
 import os
 import requests
 import json
-import random
+import random 
 from keep_alive import keep_alive
 import string
-from collections import Counter
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
 
 client = discord.Client()
 
-#sad_words = ["sad", 'depressed', 'unhappy', 'exhausted',
- #            'bored', 'miserable', 'depressing', 'depress',
-  #           'exhausted', 'tired', 'terrible', 'losing'
-   #          ]
+#sad_words = ["sad",'depressed','unhappy','exhausted','bored','miserable','depressing','depress','tired', 'terrible', 'losing','feeling down','feeling low','trouble']
 
 starter_motivator = [
-    'Cheer Up!',
-    "Always remember, I am here for you!",
-    "You are a great person. Remember this!",
-    'Think positive man! There is always a bright side!',
-    'What about you watching a funny video to swing the mood?'
-]
-
+  'Cheer Up!',
+  'Always remember, I am here for you!',
+  'You are a great person. Remember this!',
+  'Think positive man! There is always a bright side!',
+  'What about you watching a funny video to swing the mood?']
 
 def get_quote():
-    response = requests.get('https://zenquotes.io/api/random')
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q']+"  -"+json_data[0]['a']
-    return (quote)
-
+  response = requests.get('https://zenquotes.io/api/random')
+  json_data = json.loads(response.text)
+  quote= f"`{json_data[0]['q']}`"+"  -"+json_data[0]['a']
+  return (quote)
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-
+  print('Logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
@@ -52,7 +43,7 @@ async def on_message(message):
         await message.channel.send(quote)
 
     if msg.startswith('$help'):
-        await message.channel.send("This is bot help.\nCommands:\n* $hey, $hello, $hi :- Bot responds.\n* $motivate :- Generates motivating quotes.\n* $help :- Bot help.")
+        await message.channel.send("This is bot help.\nCommands:\n*` $hey, $hello, $hi `:- Bot responds.\n*` $motivate `:- Generates motivating quotes.\n*` $help `:- Bot help.")
 
 
 
@@ -65,5 +56,5 @@ async def on_message(message):
         await message.channel.send('Hey, I am sensing `Negative Sentiment` from you.\n'+f"`{random.choice(starter_motivator)}`")
     
 
-#keep_alive()
+keep_alive()
 client.run(os.environ['TOKEN'])
